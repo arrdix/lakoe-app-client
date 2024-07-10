3d61b8facff6132859849b33e6d760a7de022a7a
arrdix <mail.ardix@gmail.com>
2024-07-10 19:50:12 +0700

    chore: create message template layout

--- src/components/StoreMessageTemplate.tsx
+++ src/components/StoreMessageTemplate.tsx
@@ -1,12 +1,27 @@
 import MessageTemplateBox from '@/components/MessageTemplateBox'
+import MessageTemplateModal from '@/components/MessageTemplateModal'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
 
 function StoreMessageTemplate() {
+    const [renderModal, setRenderModal] = useState<boolean>(false)
+
+    function onRenderModal() {
+        setRenderModal((oldVal) => !oldVal)
+    }
+
+    function onModalClose() {
+        setRenderModal(false)
+    }
+
     return (
         <>
             <div className="flex justify-between items-center mt-2">
                 <h2 className="text-black text-lg font-bold">Daftar Template Pesan</h2>
-                <Button className="bg-transparent text-black border border-lightgray hover:text-white">
+                <Button
+                    className="bg-transparent text-black border border-lightgray hover:text-white"
+                    onClick={onRenderModal}
+                >
                     Buat Template
                 </Button>
             </div>
@@ -22,6 +37,33 @@ function StoreMessageTemplate() {
                 Dapatkan diskon [Persentase Diskon]% untuk pembelian di atas [Jumlah Minimum Pembelian]. 
                 Segera kunjungi toko kami dan manfaatkan promo ini!"
             />
+            <MessageTemplateBox
+                title="Pesan Promo"
+                message="Hai! Fesyen Store sedang mengadakan promo spesial! 
+                Dapatkan diskon [Persentase Diskon]% untuk pembelian di atas [Jumlah Minimum Pembelian]. 
+                Segera kunjungi toko kami dan manfaatkan promo ini!"
+            />
+            <MessageTemplateBox
+                title="Pesan Promo"
+                message="Hai! Fesyen Store sedang mengadakan promo spesial! 
+                Dapatkan diskon [Persentase Diskon]% untuk pembelian di atas [Jumlah Minimum Pembelian]. 
+                Segera kunjungi toko kami dan manfaatkan promo ini!"
+            />
+            <MessageTemplateBox
+                title="Pesan Promo"
+                message="Hai! Fesyen Store sedang mengadakan promo spesial! 
+                Dapatkan diskon [Persentase Diskon]% untuk pembelian di atas [Jumlah Minimum Pembelian]. 
+                Segera kunjungi toko kami dan manfaatkan promo ini!"
+            />
+            <MessageTemplateBox
+                title="Pesan Promo"
+                message="Hai! Fesyen Store sedang mengadakan promo spesial! 
+                Dapatkan diskon [Persentase Diskon]% untuk pembelian di atas [Jumlah Minimum Pembelian]. 
+                Segera kunjungi toko kami dan manfaatkan promo ini!"
+            />
+            {renderModal && (
+                <MessageTemplateModal isOpen={renderModal} onModalClose={onModalClose} />
+            )}
         </>
     )
 }
