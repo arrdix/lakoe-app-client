ae2d4b5eb26edcbfb9dc5730cd9d0c3f8d8bf90f
riyan <riyanmaulana04@gmail.com>
2024-07-11 13:25:56 +0700

    feat: create varian layout

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,17 +1,31 @@
-import { Wallet, Banknote, ShieldAlert, HandCoins, SquareCheckBig } from "lucide-react";
+import { Wallet, Banknote,  HandCoins, SquareCheckBig } from "lucide-react";
 import { CiCreditCard2, } from "react-icons/ci";
 import { SiContactlesspayment } from "react-icons/si";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import { FaMoneyCheckAlt } from "react-icons/fa";
 import { HiCash } from "react-icons/hi";
 import { HiOutlineDownload } from "react-icons/hi";
+import { PiExclamationMarkDuotone } from "react-icons/pi";
 import DashboardBox from "@/components/DashboardBox";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { IoMdArrowDropdown } from "react-icons/io";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import BuyerDashBoard from "./BuyerDashBoard";
 
 function DashboardPage() {
   return (
-    <div className="w-full h-full p-8 bg-lightgray">
+    <div >
+    <div className="w-full h-full p-5 0">
       <div className=" flex justify-between gap-2.5 ">
         <div className="w-full h-full bg-white w-72 h-28  ">
           <div className="p-3 ">
@@ -38,7 +52,7 @@ function DashboardPage() {
             <Banknote />
             <div className="flex justify-between">
               Saldo Tertahan
-              <ShieldAlert />
+              <PiExclamationMarkDuotone size={"2rem"} />
               <Link className="text-blue-500 " style={{fontSize:"12px"}} to="">Lihat semua</Link>
             </div>
             <div className="font-bold text-xl">Rp0</div>
@@ -66,7 +80,7 @@ function DashboardPage() {
           <DashboardBox text="Penarikan Selesai" value={0} icon={<SquareCheckBig size={"2rem"} />} />
           <DashboardBox text="Pendapatan COD" value={0} icon={<FaMoneyCheckAlt size={"2rem"} />} />
           <DashboardBox text="CashBack Pengiriman" value={0} icon={<HiCash size={"2rem"} />} />
-          <DashboardBox text="Pendapatan E-Payment" value={0} icon={<SiContactlesspayment size={"2rem"} />} />
+          <DashboardBox text="Pendapatan E-Payment" value={0} icon={<SiContactlesspayment size={"3rem"} />} />
         </div>
 
         <div className="flex">
@@ -79,11 +93,45 @@ function DashboardPage() {
 
       </div>
 
-      <div className=" w-full  bg-white mt-5 p-4  ">
+      <div className=" w-full mt-2 p-4 bg-lightgray ">
         <div className="flex justify-between  ">
-          <div className="flex gap-2 "> <HiOutlineDownload size={"1.5rem"} />Eksport</div>
-          <div>All Type</div>
-          <div>All Status</div>
+          <div className=" ">
+          <DropdownMenu>
+              <DropdownMenuTrigger className="flex"> <Button className=" bg-white text-black rounded-none  "> <HiOutlineDownload size={"1.5rem"} /> Eksport  <IoMdArrowDropdown /></Button></DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          </div>
+        
+          <DropdownMenu>
+            <DropdownMenuTrigger> <Button className=" bg-white text-black rounded-none "> All Type <IoMdArrowDropdown /></Button></DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger > <Button className=" bg-white text-black rounded-none "> All Status <IoMdArrowDropdown /></Button></DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Input className="w-40 rounded-none" placeholder="Order Id" type="text"/>
         </div>
       </div>
 
@@ -99,6 +147,10 @@ function DashboardPage() {
       </div>
 
 
+    </div>
+<div>
+  <BuyerDashBoard/>
+</div>
     </div>
   );
 }
