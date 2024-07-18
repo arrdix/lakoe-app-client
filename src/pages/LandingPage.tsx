255d6c1ad2f536b1b64b4e3969467bab64ae827b
arrdix <mail.ardix@gmail.com>
2024-07-19 02:00:58 +0700

    feat: implement buyer feature

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -18,8 +18,9 @@ export default function LandingPage() {
 
     useEffect(() => {
         async function GET_PRODUCTS() {
-            const products = await API.PRODUCT.GET_ALL_BY_SKU()
+            const products = await API.PRODUCT.GET_ALL_BY_ID()
             setProducts(products)
+            console.log(products)
         }
 
         GET_PRODUCTS()
