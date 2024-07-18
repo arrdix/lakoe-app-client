58ff86cd0e397cfb5ac1a13c217b1484800ef94e
aditdwiramadhan123 <aditdwiramadhan@students.unnes.ac.id>
2024-07-18 11:37:04 +0700

    feat: product list feature

--- src/components/product/CardFooter.tsx
+++ src/components/product/CardFooter.tsx
@@ -11,8 +11,8 @@ interface CardFooterProps {
 export default function CardFooter({ productName,productSku }: CardFooterProps) {
     return (
         <div className="flex flex-row gap-2">
-            <ChangeProductPriceModal productName={productName} />
-            <ChangeProductStockModal productName={productName} />
+            <ChangeProductPriceModal productName={productName} productSku={productSku} />
+            <ChangeProductStockModal productName={productName} productSku={productSku}  />
             <Button variant={'outline'} className="text-xs">
                 <BiLink className="mr-1" />
                 Lihat Halaman
